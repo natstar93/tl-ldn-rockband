d8dd5d4c05a5fefa1cda63d4ddb1c22b855b2a7a
Natalie Akam <Natalie.Akam@thetrainline.com>
2017-12-30 16:33:29 +0000

    Extend getTracks function to get more than 100 results

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ from json import loads
 
 def getTracks(url):
     print('\n>> Getting tracks from {0}'.format(url))
-    token = 'Bearer BQAHUlzhOtl_9NLFCXVdb2tmOB69nfB5_tlwrod17Y8axtoa0iyQHH9HYaPtjGd9bAkbwJ1910xiUCnXq7KXRn8zWrSGA8HXm3qMsZVLvuIJQ9fYIpNkPInsZ88XeKkl3lEKR32OX_dBwdo-k1l25CNghqJlPNCqDQ'
-    request = Request(url, headers={'Authorization': token})
+    token = 'Bearer BQC9Jyw32zS1wDHeq0poASw2ohB7RYElexrKlGZfM5f2g8KmrwBtBEFAgUT1vRC8UWM2uXzGzBi0TTfoMyf3O8LeA08tKJeGZzusY8K2ijYTyi2jvaOnbDeSqPFohqHjoJAAf5oyBn1idxjpmWwnaU3ObaH3AJ4oYA'
     try:
-        response = urlopen(request)
-        data = response.read()
-        encoding = response.info().get_content_charset('utf8')
-        decodedData = loads(data.decode(encoding))
-        return decodedData['items']
+        tracks = []
+        while url:
+            request = Request(url, headers={'Authorization': token})
+            response = urlopen(request)
+            data = response.read()
+            encoding = response.info().get_content_charset('utf8')
+            decodedData = loads(data.decode(encoding))
+            tracks = tracks + decodedData['items']
+            url = decodedData['next']
+        return tracks
     except(URLError, e):
         print('Fail', e)
 
 def main():
-    tlUrl = 'https://api.spotify.com/v1/users/robcthegeek/playlists/2JwE2prZ0fdX82d3alpGhQ/tracks?fields=items(track(id,name,artists(name)),added_by(id)),next&offset=00'
-    rbUrl = 'https://api.spotify.com/v1/users/rockbandofficial/playlists/1LOZfgjinUc6K2mz8wjPz3/tracks?fields=items(track(id)),next&offset=600'
+    tlUrl = 'https://api.spotify.com/v1/users/robcthegeek/playlists/2JwE2prZ0fdX82d3alpGhQ/tracks?fields=items(track(id,name,artists(name)),added_by(id)),next'
+    rbUrl = 'https://api.spotify.com/v1/users/rockbandofficial/playlists/1LOZfgjinUc6K2mz8wjPz3/tracks?fields=items(track(id)),next'
 
     tlTracks = getTracks(tlUrl)
     rbTracks = getTracks(rbUrl)
