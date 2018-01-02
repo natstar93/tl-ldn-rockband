9cc573dac6210c105cd04f133502d00cb096e92b
Natalie Akam <natalie.akam@gmail.com>
2018-01-02 21:05:23 +0000

    Update Spotify authorisation and get playlist name

--- main.py
+++ main.py
@@ -8,72 +8,7 @@ import http.server
 import webbrowser
 import re
 import sys
-
-class SpotifyAPI:
-	def __init__(self, auth):
-		self._auth = auth
-
-	def getTracks(self, url):
-		print('\n>> Getting tracks from {0}'.format(url))
-		tracks = []
-		try:
-			while url:
-				decodedData = getData(url, {'Authorization': 'Bearer ' + self._auth})
-				tracks.extend(decodedData['items'])
-				url = decodedData['next']
-		except Exception:
-			print('\nFailed to get Spotify tracks')
-		return tracks
-
-	@staticmethod
-	def webAuthorize(client_id):
-		webbrowser.open('https://accounts.spotify.com/authorize?' + urllib.parse.urlencode({
-			'response_type': 'token',
-			'client_id': client_id,
-			'scope': '',
-			'redirect_uri': 'http://127.0.0.1:{}/redirect'.format(SpotifyAPI._SERVER_PORT)
-		}))
-
-		server = SpotifyAPI._AuthorizationServer('127.0.0.1', SpotifyAPI._SERVER_PORT)
-		try:
-			while True:
-				server.handle_request()
-		except SpotifyAPI._Authorization as auth:
-			return SpotifyAPI(auth.access_token)
-
-	_SERVER_PORT = 43019
-
-	class _AuthorizationServer(http.server.HTTPServer):
-		def __init__(self, host, port):
-			http.server.HTTPServer.__init__(self, (host, port), SpotifyAPI._AuthorizationHandler)
-
-		def handle_error(self, request, client_address):
-			raise
-
-	class _AuthorizationHandler(http.server.BaseHTTPRequestHandler):
-		def do_GET(self):
-			if self.path.startswith('/redirect'):
-				self.send_response(200)
-				self.send_header('Content-Type', 'text/html')
-				self.end_headers()
-				self.wfile.write(b'<script>location.replace("token?" + location.hash.slice(1));</script>')
-
-			elif self.path.startswith('/token?'):
-				self.send_response(200)
-				self.send_header('Content-Type', 'text/html')
-				self.end_headers()
-				self.wfile.write(b'<script>close()</script>Thanks! You may now close this window.')
-				raise SpotifyAPI._Authorization(re.search('access_token=([^&]*)', self.path).group(1))
-
-			else:
-				self.send_error(404)
-
-		def log_message(self, format, *args):
-			pass
-
-	class _Authorization(Exception):
-		def __init__(self, access_token):
-			self.access_token = access_token
+import requests
 
 def getData(url, headers={}):
     try:
@@ -86,17 +21,59 @@ def getData(url, headers={}):
         print('\nFailed to get data from', url)
 
 def createSpotifyUrl(url):
-    return url.replace('https://open.spotify.com/user', 'https://api.spotify.com/v1/users').replace('playlist', 'playlists')
+    urlComponents = {
+        'https://open.spotify.com/user': 'https://api.spotify.com/v1/users',
+        'playlist': 'playlists'
+    }
+
+    for component in urlComponents:
+        url = url.replace(component, urlComponents[component])
+    return url
+
+def getSpotifyTracks(url, token):
+    tracks = []
+    try:
+        while url:
+            decodedData = getData(url, {'Authorization': 'Bearer ' + token})
+            tracks.extend(decodedData['items'])
+            url = decodedData['next']
+    except Exception:
+        print('\nFailed to get Spotify tracks')
+    return tracks
+
+def getSpotifyPlaylistName(url, token):
+    try:
+        data = getData(url + '?fields=name', {'Authorization': 'Bearer ' + token})
+        return data['name']
+    except Exception:
+        return 'Spotify playlist'
+
+def getSpotifyAuthToken():
+    client_id = 'fcc8cc664f5f448e9c90b265a77118a5'
+    client_secret = 'b651409975ea4a129347d1c7c603c070'
+
+    grant_type = 'client_credentials'
+
+    body_params = {'grant_type' : grant_type}
+
+    authUrl='https://accounts.spotify.com/api/token'
+
+    response = requests.post(authUrl, data=body_params, auth = (client_id, client_secret))
+    return response.json()['access_token']
 
 def main():
-    spotify = SpotifyAPI.webAuthorize(client_id='fcc8cc664f5f448e9c90b265a77118a5')
-
     tlPlaylistUrl = 'https://api.spotify.com/v1/users/robcthegeek/playlists/2JwE2prZ0fdX82d3alpGhQ'
 
     spotifyPlaylistUrl = createSpotifyUrl(sys.argv[1]) if len(sys.argv) > 1 else tlPlaylistUrl
     rockbandPlaylistUrl = 'https://rbdb.io/v3/songs?fields=spotifyId'
 
-    tlTracks = spotify.getTracks(spotifyPlaylistUrl + '/tracks?fields=items(track(id,name,artists(name)),added_by(id)),next')
+    spotifyToken = getSpotifyAuthToken()
+
+    spotifyPlaylistName = getSpotifyPlaylistName(spotifyPlaylistUrl, spotifyToken)
+
+    print('\n>> Getting tracks from {0}'.format(spotifyPlaylistName))
+
+    spotifyTracks = getSpotifyTracks(spotifyPlaylistUrl + '/tracks?fields=items(track(id,name,artists(name)),added_by(id)),next', spotifyToken)
 
     try:
         rbTracks = getData(rockbandPlaylistUrl)['collection']
@@ -105,19 +82,17 @@ def main():
         print('\nFailed to get RockBand tracks')
         rbIds = []
 
-    matches = []
+    numberOfMatches = 0
 
-    for tlTrack in tlTracks:
-        if any(rbId == tlTrack['track']['id'] for rbId in rbIds):
-            matches.append(tlTrack)
+    for track in spotifyTracks:
+        if any(rbId == track['track']['id'] for rbId in rbIds):
+            print('\n* {track[name]} by {track[artists][0][name]}. Added by {added_by[id]}.'.format(**track))
+            numberOfMatches += 1
 
-    for track in matches:
-        print('\n*** Match: {track[name]} by {track[artists][0][name]}. Added by {added_by[id]}.'.format(**track))
-
-    if len(matches) == 0:
+    if numberOfMatches == 0:
         print('\n** No matches :(')
     else:
-        print('\n*** {0} matches found. ***'.format(len(matches))) # pluralisation (1 matches?)
+        print('\n*** {0} Rock Band tracks found in {1}. ***'.format(numberOfMatches, spotifyPlaylistName)) # pluralisation (1 matches?)
 
 if __name__ == '__main__':
     main()
